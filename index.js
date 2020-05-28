85da157ae3ae2be328cade044ec349aae4f16a52
Matheus Vieira <matheus_lemes1996@hotmail.com>
2020-05-28 15:03:39 -0300

    Fixing view paths

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const dbConnection = sqlite.open({
 
 const port = process.env.PORT || 3000
 
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
